f825a40bb18cc70858e78dec7358d12b47c36992
Italo Rangel <italopenaforte@gmail.com>
2022-10-24 19:43:44 -0300

    feat: build the base struct for the AST

---
+++ ast/ast.go
@@ -0,0 +1,46 @@
+package ast
+
+import "github.com/italopenaforte/monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statement []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statement) > 0 {
+		return p.Statement[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
