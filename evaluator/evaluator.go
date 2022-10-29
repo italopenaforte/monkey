e1cd30e47d88767de4401cd35713997a3369ecd4
Italo Rangel <italopenaforte@gmail.com>
2022-10-29 19:43:55 -0300

    feat: make function evaluate and return the result

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -65,8 +65,69 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		// huh/ now what?
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
+
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+		return applyFunction(function, args)
 	}
+
 	return nil
+}
+
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+	return unwrapReturnValue(evaluated)
+
+}
+func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	for paramIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+
+	return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	returnValue, ok := obj.(*object.ReturnValue)
+
+	if ok {
+		return returnValue.Value
+	}
+
+	return obj
+}
+
+func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
+	var result []object.Object
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+	return result
+
 }
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
@@ -237,7 +298,9 @@ func evalStatements(stmts []ast.Statement, env *object.Environment) object.Objec
 	for _, statement := range stmts {
 		result = Eval(statement, env)
 
-		if returnValue, ok := result.(*object.ReturnValue); ok {
+		returnValue, ok := result.(*object.ReturnValue)
+
+		if ok {
 			return returnValue.Value
 		}
 	}
