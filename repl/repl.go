57d69819073edc7e2b628714c931c78a515d96f4
Italo Rangel <italopenaforte@gmail.com>
2022-10-29 19:09:25 -0300

    feat: make tracking of tree execution

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/italopenaforte/monkey/evaluator"
 	"github.com/italopenaforte/monkey/lexer"
+	"github.com/italopenaforte/monkey/object"
 	"github.com/italopenaforte/monkey/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -30,7 +32,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
